0f084072cae006763c6a2b00e97386cde1c68345
Alladio Bonesso <alladio_bonesso@hotmail.com>
2021-07-17 01:43:14 -0300

    first commit

---
+++ main.ts
@@ -0,0 +1,21 @@
+var minhaVar = "Minha Variável";
+
+function minhaFunc(x, y){
+    return x+y;
+}
+
+let num =2;
+const Pi = 3.14;
+var numeros =[1,2,3];
+numeros.map(function(valor){
+    return valor*2;
+});
+
+numeros.map(valor => valor *2);
+
+class Matematica{
+    soma(x,y){
+        return x=y;
+    }
+
+}
